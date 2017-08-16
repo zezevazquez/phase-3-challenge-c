7249492a76a058d9f491d90e94ac3cb5bf38ff0b
zezemanolo <manoloish@gmail.com>
2017-08-16 09:26:50 -0700

    removes commented notes from schema

--- part-2/sql/schema.sql
+++ part-2/sql/schema.sql
@@ -16,16 +16,3 @@ CREATE TABLE rooms(
   room_number VARCHAR(100) NOT NULL,
   capacity INTEGER NOT NULL
 );
-
-
--- CASE
--- 		WHEN capacity > 2 THEN true
--- 		ELSE false
--- 	END
---
---   SELECT room_number, capacity,
--- CASE
--- 	WHEN date > check_in AND date < check_out THEN 'UNAVAILABLE'
--- 	ELSE 'AVAILAVLE'
--- END
--- FROM rooms JOIN bookings ON bookings.room_id = rooms.id
